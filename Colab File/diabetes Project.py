ae0da35ec97b9e2453b6429a0691245434c58e6b
Hustle <farhansadiknihal@gmail.com>
2025-12-25 20:21:15 +0600

    Data Pre Processing Done

---
+++ Colab File/diabetes Project.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix, roc_curve, auc
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+
+
+
+
+df = pd.read_csv('F:\Pictures, Certificate\Hustle\Hustle in Coding\Java Coding\Versity\Versity Project\CSE422 ML\Main Project\Dataset\diabetes_dataset.csv')
+
+# df.info()
+df = df.drop_duplicates()
+# df.info()
+# df.describe()
+
+colmnsNumerical = ['age', 'bmi', 'HbA1c_level', 'blood_glucose_level', 'hypertension', 'heart_disease']
+for col in colmnsNumerical:
+  df[col].fillna(df[col].median(), inplace=True)
+
+colmnsCatagorical = ['gender', 'smoking_history']
+for col in colmnsCatagorical:
+  df[col].fillna(df[col].mode()[0], inplace=True)
+
+df.dropna(subset=['diabetes'], inplace=True)
+
+df.info()
+df.describe()
